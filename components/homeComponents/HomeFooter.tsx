6cc15ccfd7606f5b4cdd39993aa6bdbe138dff80
JonathonDavis <112988057+JonathonDavis@users.noreply.github.com>
2023-10-08 15:42:11 -0500

    feat(main home page): Changed some front end stuff
    Attempted to bring the frontend of the homepage closer to the draft envisionsed

--- components/homeComponents/HomeFooter.tsx
+++ components/homeComponents/HomeFooter.tsx
@@ -6,7 +6,13 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 export default function HomeFooter() {
   return (
-    <section className=" mt-16 px-6 py-8 md:text-base text-xs">
+    <section
+      className=" mt-16 px-6 py-8 md:text-base text-xs"
+      style={{
+        backgroundImage:
+          "url('https://firebasestorage.googleapis.com/v0/b/hacksa-62c97.appspot.com/o/background_images%2Ftransparent_purpleBG.png?alt=media&token=d5ae999d-54aa-462c-a3f1-bf586de7800a&_gl=1*9e1u44*_ga*MTA5NjM2NjQ3My4xNjk2MDU0MDg2*_ga_CW55HF8NVT*MTY5Njc5MjM5My4xOC4xLjE2OTY3OTQ3NjMuNDkuMC4w')",
+      }}
+    >
       <hr className="my-4 bg-complementary" />
       <div className="flex flex-col items-center justify-center gap-2 text-complementary text-center">
         <div className="text-base md:text-lg">
@@ -23,7 +29,7 @@ export default function HomeFooter() {
           </a>
         </div>
         <div className="text-[0.6rem] md:text-sm">
-          Designed by <p className="font-black inline">HackUTD | </p>
+          Designed by <p className="font-black inline">ACM Tamusa | </p>
           {/* PLEASE DO NOT CHANGE <3 */}
           HackPortal developed with &lt;3 by <p className="font-black inline">HackUTD</p> and{' '}
           <p className="font-black inline">ACM Development</p>
@@ -68,7 +74,6 @@ export default function HomeFooter() {
           >
             <FontAwesomeIcon icon={faLinkedin} height={36} />
           </a>
-          {/* SHOULD WE ADD ANY MORE SOCIAL LINKS DOWN HERE?*/}
         </div>
       </div>
     </section>
--- components/homeComponents/HomeHero.tsx
+++ components/homeComponents/HomeHero.tsx
@@ -1,24 +1,37 @@
 import { useRouter } from 'next/router';
 import { buttonDatas } from '../../lib/data';
+import Image from 'next/image';
 
 export default function HomeHero() {
   const router = useRouter();
 
   return (
-    <section className="min-h-screen p-4 bg-contain bg-white">
+    <section
+      className="min-h-screen p-4 bg-gradient-to-t from-purple-900 to-purple-5"
+      style={{
+        backgroundImage:
+          "url('https://firebasestorage.googleapis.com/v0/b/hacksa-62c97.appspot.com/o/background_images%2Ftransparent_purpleBG.png?alt=media&token=d5ae999d-54aa-462c-a3f1-bf586de7800a&_gl=1*9e1u44*_ga*MTA5NjM2NjQ3My4xNjk2MDU0MDg2*_ga_CW55HF8NVT*MTY5Njc5MjM5My4xOC4xLjE2OTY3OTQ3NjMuNDkuMC4w')",
+      }}
+    >
       <div
         style={{ minHeight: 480 }}
         className="max-w-4xl mx-auto flex flex-col justify-center items-center"
       >
-        <h1 className="text-center md:text-8xl text-6xl font-bold text-primaryDark">HackSA</h1>{' '}
+        {/* TAMUSA OUTLINE IMAGE */}
+        <Image
+          src="https://firebasestorage.googleapis.com/v0/b/hacksa-62c97.appspot.com/o/background_images%2Fpurple_Tamusa_outline.png?alt=media&token=9349c32f-34a5-41c7-9e91-057183239992&_gl=1*patkrz*_ga*MTA5NjM2NjQ3My4xNjk2MDU0MDg2*_ga_CW55HF8NVT*MTY5Njc0NjAwNi4xNS4xLjE2OTY3NDYwMTQuNTIuMC4w"
+          alt="Tamusa Outline"
+          width={1000}
+          height={775}
+        />
+        <h1 className="text-center md:text-8xl text-6xl font-bold text-white">HackSA</h1>{' '}
         {/* !change */}
-        <p className="text-center my-4 font-semibold md:text-xl text-md text-primaryDark opacity-80">
+        <p className="text-center my-4 font-semibold md:text-xl text-md text-white opacity-100">
           {' '}
-          {/* !change */}Hacks for social good
+          {/* !change */}October 23rd - 24th
         </p>
       </div>
       {/* TODO: Programmatically show these based on configured times/organizer preference */}
-
       <div className="flex flex-col items-center md:flex-row md:justify-around px-44 md:space-y-0 space-y-3 > *">
         {buttonDatas.map((button) => (
           <button
