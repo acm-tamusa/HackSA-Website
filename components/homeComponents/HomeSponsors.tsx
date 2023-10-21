656734318b8aea502f8c9ce9be85a668f8ad8e89
JonathonDavis <112988057+JonathonDavis@users.noreply.github.com>
2023-10-21 14:20:31 -0500

    style(homesponsors.tsx): Change the formating of sponsors/partners

--- components/homeComponents/HomeSponsors.tsx
+++ components/homeComponents/HomeSponsors.tsx
@@ -21,7 +21,7 @@ export default function HomeSponsors(props: { sponsorCard: Sponsor[] }) {
       >
         <div className="flex flex-col flex-grow bg-customPurple2 bg-opacity-40 rounded-md">
           <h4 className="text-complementary font-bold text-white md:text-4xl text-2xl my-4 px-4">
-            Sponsors/Partners
+            Sponsors / Partners
           </h4>
           {/* Sponsor Card */}
           {/* <section className="flex justify-center p-4 rounded-md bg-gradient-to-tr from-purple-500 to-pink-600" */}
